08f87aa0a00426b45c2685e0873be4713af6e0b6
Charlie Nash <charlie.tc.nash@gmail.com>
2015-11-19 22:15:18 +0000

    Documentation added to nricp.m

--- demos/faceDemo.m
+++ demos/faceDemo.m
@@ -7,5 +7,8 @@ load data/faceTarget.mat
 % Specify that surface normals are available and can be used.
 Options.useNormals = 1;
 
+% Specify that the source deformations should be plotted.
+Options.plot = 1;
+
 % Perform non-rigid ICP
 [pointsTransformed, X] = nricp(Source, Target, Options);
